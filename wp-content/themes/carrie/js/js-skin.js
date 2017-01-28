7ee191b88985d7096de6c880c07437ae63bb5f10
René A. Morales <rene.morales.sanchez@gmail.com>
2017-01-28 13:12:48 -0300

    caca

---
+++ wp-content/themes/carrie/js/js-skin.js
@@ -0,0 +1,8 @@
+    (function($){
+    $(document).ready(function() {
+        
+        
+        
+    });
+    })(jQuery);
+     /*2017-01-28 15:28*//* JS Generator Execution Time: 0.000174999237061 seconds */
